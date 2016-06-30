b86356aab794de88eb642b3fd832cfe0e5f9de6b
jrnorcliffe <ee13jrn@leeds.ac.uk>
2016-06-30 16:20:51 +0100

    correctio to mod_tests

--- mod_tests.py
+++ mod_tests.py
@@ -5,5 +5,5 @@ from mean import *
 def test_mean_1234():
     num_list = [1,2,3,4]
     calc_mean = mean(num_list)
-    expected_mean = 3
+    expected_mean = 2.5
     assert expected_mean == calc_mean
